9322b5b4e6781f74bc89c055f83c1c8cdca9d48b
DESKTOP-DVVSLFI\Marko <superfreshdev@gmx.de>
2026-06-15 22:47:21 +0200

    update

--- js/02-webpage/01-default/header/header-body.js
+++ js/02-webpage/01-default/header/header-body.js
@@ -70,7 +70,7 @@ for( let i=0; i < radios_header_body.length; i++ ) {
 
     } else {
 
-      var updateCssStyle = "color:rgb(124, 124, 124); border: 2px solid transparent; "
+      var updateCssStyle = "rgb(134, 134, 134); background-color: rgb(44, 44, 44); border: 2px solid rgb(56, 56, 56);"
       await async_update_cssStyle_to_element( updateCssStyle, divCompanyContainer )
 
     }
